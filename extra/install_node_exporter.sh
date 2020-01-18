a3321ee7cf789495030e07f8a872aee3dcbcb723
Paulo Roberto <paulo.roberto@live.com>
2020-01-18 23:06:57 +0000

    Corrigindo sintaxe dos comando

--- extra/install_node_exporter.sh
+++ extra/install_node_exporter.sh
@@ -5,10 +5,10 @@ sudo tar -zxf node_exporter-0.15.1.linux-amd64.tar.gz
 cd node_exporter-0.15.1.linux-amd64
 sudo cp node_exporter /usr/local/bin/
 sudo useradd node_exporter
-sudo chwon node_exporter:node_exporter /usr/local/bin/node_exporter
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 cd ../
 rm -f node_exporter-0.15.1.linux-amd64.tar.gz
 sudo aws s3 cp s3://projeto05-transfers/node_exporter/node_exporter.service /etc/systemd/system/node_exporter.service
 sudo systemctl daemon-reload
-sudo sytemctl start node_exporter
-sudo sytemctl enable node_exporter
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
