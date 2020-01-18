df09743d4e4c64a61657ef3096fc96dba24218f7
Paulo Roberto <paulo.roberto@live.com>
2020-01-18 19:18:25 +0000

    Script para instalar e configurar o Node Exporter nas máquinas

---
+++ extra/install_node_exporter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.15.1/node_exporter-0.15.1.linux-amd64.tar.gz
+sudo tar -zxf node_exporter-0.15.1.linux-amd64.tar.gz
+cd node_exporter-0.15.1.linux-amd64
+sudo cp node_exporter /usr/local/bin/
+sudo useradd node_exporter
+sudo chwon node_exporter:node_exporter /usr/local/bin/node_exporter
+cd ../
+rm -f node_exporter-0.15.1.linux-amd64.tar.gz
+sudo aws s3 cp s3://projeto05-transfers/node_exporter/node_exporter.service /etc/systemd/system/node_exporter.service
+sudo systemctl daemon-reload
+sudo sytemctl start node_exporter
+sudo sytemctl enable node_exporter
